efb70c730aa8d64b433ba25831871c8c8be1309d
Lin <toddtodd987@gmail.com>
2026-04-06 16:13:04 +0800

    first

---
+++ code.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+class CSTACK
+{
+private:
+	char *A;
+	int size;
+	int index;
+public:
+	CSTACK(int s) {
+		size = s;
+		index = 0;
+		A = new char[size];
+	}
+
+	~CSTACK() {
+		delete[] A;
+	}
+	bool isEmpty()
+	{
+		if (0  >= index) return true;
+		else return false;
+	}
+	bool isFull()
+	{
+		if (size <= index) return true;
+		else return false;
+	}
+	bool push(char data)
+	{
+		if (isFull()) return false;
+		else
+		{
+			A[index] = data;
+			index = index + 1;
+			return true;
+		}
+	}
+
+	bool pop(char& data)
+	{
+		if (isEmpty()) return false;
+		else
+		{
+			index--;
+			data = A[index];
+			return true;
+		}
+	}
+
+	void show()
+	{
+		int i;
+		for (i = 0; i < index; i++)
+			cout << A[i];
+		cout << endl;
+
+	}
+
+};
+int main()
+{
+	char data;
+	CSTACK s1(100);
+	CSTACK s2(80);
+	s1.push('a'); s1.show();
+	s2.push('X'); s2.show();
+	s1.push('b'); s1.show();
+	s1.pop(data); s1.show();
+	s1.push('c'); s1.show();
+	s2.push('Y'); s2.show();
+	s1.push('d'); s1.show();
+	
+}
